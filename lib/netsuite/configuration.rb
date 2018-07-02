36247b4fb1cb32f8973d76d905957331e18893ac
Kenny <bergquist.kenneth@gmail.com>
2018-07-02 16:12:53 -0400

    set host to fix datacenter errors

--- lib/netsuite/configuration.rb
+++ lib/netsuite/configuration.rb
@@ -17,7 +17,7 @@ module NetSuite
     def connection(params={}, credentials={})
       client = Savon.client({
         wsdl: cached_wsdl || wsdl,
-        endpoint: wsdl_endpoint,
+        host: wsdl_endpoint,
         read_timeout: read_timeout,
         namespaces: namespaces,
         soap_header: auth_header(credentials).update(soap_header),
